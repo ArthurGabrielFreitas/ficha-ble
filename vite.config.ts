3ca460bffe60319c9530291a9e3e3eb32070bbbf
ArthurGabrielFreitas <artgabrielfdm@gmail.com>
2025-06-09 15:15:03 -0300

    Atualização de packages

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: '/fichable/', // ⚠️ importante: com barra no início e no fim
+  base: '/ficha-ble/', // ⚠️ importante: com barra no início e no fim
 });
